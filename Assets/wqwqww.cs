0f1d0765378406132b18403a7836c48e69733fb6
echo <794089512@qq.com>
2022-01-15 14:24:47 +0800

    no message

--- Assets/wqwqww.cs
+++ Assets/wqwqww.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class wqwqww : MonoBehaviour
 {
     public GameObject i;
-  
+
+    
     // Start is called before the first frame update
     void Start()
     {
         i = GameObject.Find("Image");
         i.SetActive(false);
+        Debug.Log("I am alive! ");
+        static void fucntion1()
+        {
 
+        };
+        void function2()
+        {
+
+        };
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0)) ;
+        if (Input.anyKey) ;
         i.SetActive(true);
     }
 }
